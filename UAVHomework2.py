9be88fd0c493ad10ba6fc5022748b3087a58d25a
Voron <mcbleshned@gmail.com>
2023-11-17 12:02:29 +0500

    feat: homework2 completed add async functional

--- UAVHomework2.py
+++ UAVHomework2.py
@@ -1,4 +1,3 @@
-# TODO set up async
 import ftplib
 import filecmp
 import asyncio
@@ -6,20 +5,23 @@ import asyncio
 
 async def push_item(ftp_connection: ftplib.FTP, item_path: str) -> None:
     with open(file=item_path, mode='rb') as file:
-        await ftp_connection.storbinary(cmd=' '.join(('STOR', item_path)), fp=file, blocksize=1024)
+        await asyncio.sleep(1)
+        ftp_connection.storbinary(cmd=' '.join(('STOR', item_path)), fp=file, blocksize=1024)
 
 
 async def receive_item(ftp_connection: ftplib.FTP, item_path: str, receive_path: str) -> None:
     with open(file=receive_path, mode='wb') as file:
-        await ftp_connection.retrbinary(cmd=' '.join(('RETR', item_path)), callback=file.write)
+        await asyncio.sleep(1)
+        ftp_connection.retrbinary(cmd=' '.join(('RETR', item_path)), callback=file.write)
 
 
-async def run():
+async def async_run():
     ftp = connection()
     task1 = push_item(ftp, 'UAVHomework1.py')
     task2 = receive_item(ftp, 'UAVHomework1.py',
                          r'D:\Program Files\JetBrains\PythonProjects\studies-programming-uav\instance.py')
-    await asyncio.wait((task1, task2))
+    await task1
+    await task2
     ftp.close()
 
 
@@ -32,7 +34,7 @@ def connection() -> ftplib.FTP:
 
 
 def main():
-    run()
+    asyncio.run(async_run())
     if filecmp.cmp('UAVHomework1.py', 'instance.py'):
         print('Files are identical')
     else:
